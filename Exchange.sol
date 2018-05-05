1f7565193586fde60e6d44920ab725e70e546249
earth2378 <earth2378@hotmail.com>
2018-05-05 14:59:08 +0700

    Rearrange farm product's ownership collection
    Change how to collect farm product's ownership from uint to new object

--- Exchange.sol
+++ Exchange.sol
@@ -3,21 +3,29 @@ pragma solidity ^0.4.20;
 contract Exchange{
 
     //This struct represents product from farm.
-    //farmProductTag: Tag of farmer's product
-    //amount        : Amount of product
-    //owner         : owner of product
+    //farmProductId : Id of this farmer's product
+    //farmProductTag: Tag to identify farmer's product
+    //owner         : current owner of this product
+    //totalSupply   : Total supply of this product
+    //currentSupply : Supply remaining on this product
+    //parentFarmProduct : Identify which farmProductId that this product obtain from
+    //childFarmProduct  : Identify where farm products are divide to
     struct FarmProduct{
+        bytes32 farmProductId;
         bytes32 farmProductTag;
-        uint amount;
         address owner;
+        uint totalSupply;
+        uint currentSupply;
+        bytes32 parentFarmProduct;
+        bytes32[] childFarmProduct;
     }
     address FARMER_CONTRACT;
 
-    //This mapping represents farmProduct usually for farmer to sell to distributor
+    //This mapping represents farmProduct by mappin farmProductId to it's information
     mapping(bytes32 => FarmProduct) farmProduct;
 
-    //This mapping represents distributor's farm product that hold
-    mapping(address => mapping(bytes32 => uint)) distributor;
+    //This mapping represents distributor's farm product that he or she had  owned
+    mapping(address => bytes32[]) distributorFarmProductOwn;
 
     //Constructure
     function Exchange()public{
@@ -31,33 +39,57 @@ contract Exchange{
     }
 
     //This function use to add harvested product to this contract
-    function addProduct(bytes32 _farmProductTag, uint _amount, address _owner,address _FARMER_CONTRACT)public{
+    function addProduct(bytes32 _farmProductTag, uint _totalSupply,address _FARMER_CONTRACT)public{
         require(_FARMER_CONTRACT == FARMER_CONTRACT);
-        farmProduct[_farmProductTag].farmProductTag = _farmProductTag;
-        farmProduct[_farmProductTag].amount = _amount;
-        farmProduct[_farmProductTag].owner = _owner;
+        bytes32 _farmProductId = keccak256(_farmProductTag,msg.sender,_totalSupply,now);
+        farmProduct[_farmProductId].farmProductId = _farmProductId;
+        farmProduct[_farmProductId].farmProductTag = _farmProductTag;
+        farmProduct[_farmProductId].owner = msg.sender;
+        farmProduct[_farmProductId].totalSupply = _totalSupply;
+        farmProduct[_farmProductId].currentSupply = _totalSupply;
+    }
+    
+    //This function use to create new farmProduct that is divided from the other.
+    function divideProduct(bytes32 _farmProductId, uint _amount)public returns(bytes32){
+        bytes32 _newFarmProductId = keccak256(_farmProductId,msg.sender,_amount,now);
+        require(farmProduct[_newFarmProductId].owner == address(0));
+        require(farmProduct[_farmProductId].currentSupply >= _amount);
+        
+        farmProduct[_newFarmProductId].farmProductId = _newFarmProductId;
+        farmProduct[_newFarmProductId].farmProductTag = farmProduct[_farmProductId].farmProductTag;
+        farmProduct[_newFarmProductId].owner = msg.sender;
+        farmProduct[_newFarmProductId].totalSupply = _amount;
+        farmProduct[_newFarmProductId].parentFarmProduct = _farmProductId;
+        farmProduct[_newFarmProductId].currentSupply = _amount;
+        
+        farmProduct[_farmProductId].currentSupply -= _amount;
+        farmProduct[_farmProductId].childFarmProduct.push(_newFarmProductId);
+        
+        return _newFarmProductId;
+        
     }
 
     //This function is for distributor who wwant to buy product from farmer
-    function buyFromFarmer(bytes32 _farmProductTag, uint _amount)public payable{
-        require(farmProduct[_farmProductTag].amount >= _amount);
-        farmProduct[_farmProductTag].amount -= _amount;
-        distributor[msg.sender][_farmProductTag] += _amount;
-        farmProduct[_farmProductTag].owner.transfer(msg.value);
+    function buyFarmProduct(bytes32 _farmProductId, uint _amount)public payable{
+        require(farmProduct[_farmProductId].currentSupply >= _amount);
+        
+        distributorFarmProductOwn[msg.sender].push(divideProduct(_farmProductId,_amount));
+        farmProduct[_farmProductId].owner.transfer(msg.value);
     }
 
     //This function is for distributor who wwant to buy product from other distributor
-    function buyFromDistributor(bytes32 _farmProductTag, uint _amount, address _owner)public payable{
-        require(distributor[_owner][_farmProductTag] >= _amount);
-        distributor[_owner][_farmProductTag] -= _amount;
-        distributor[msg.sender][_farmProductTag] += _amount;
-        _owner.transfer(msg.value);
-    }
+    // function buyFromDistributor(bytes32 _farmProductId, uint _amount, address _owner)public payable{
+        
+    //     require(distributor[_owner][_farmProductId] >= _amount);
+    //     distributor[_owner][_farmProductId] -= _amount;
+    //     distributor[msg.sender][_farmProductId] += _amount;
+    //     _owner.transfer(msg.value);
+    // }
 
     //This function use to burn product when sell to retail store such as top
-    function retailStore(bytes32 _farmProductTag, uint _amount)public{
-        require(distributor[msg.sender][_farmProductTag]  >= _amount);
-        distributor[msg.sender][_farmProductTag] -= _amount;
+    function retailStore(bytes32 _farmProductId, uint _amount)public{
+        require(farmProduct[_farmProductId].currentSupply >= _amount);
+        farmProduct[_farmProductId].currentSupply -= _amount;
     }
 
 }
